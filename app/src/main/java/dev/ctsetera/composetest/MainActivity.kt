0ae9412d17c9504900f40b7a725b092740e597da
WORKSTATION\ctsetera <ctsetera@proton.me>
2024-11-17 17:18:50 +0900

    リスト画面をProfileListScreen.ktに切り出し＆ポトフちゃんとワトラちゃんを追加

--- app/src/main/java/dev/ctsetera/composetest/MainActivity.kt
+++ app/src/main/java/dev/ctsetera/composetest/MainActivity.kt
@@ -4,19 +4,9 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.layout.width
-import androidx.compose.foundation.shape.CircleShape
-import androidx.compose.material3.Button
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
@@ -30,11 +20,9 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.clip
-import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
 import dev.ctsetera.composetest.ui.screen.OnboardingScreen
+import dev.ctsetera.composetest.ui.screen.ProfileListScreen
 import dev.ctsetera.composetest.ui.theme.ComposeTestTheme
 
 class MainActivity : ComponentActivity() {
@@ -48,12 +36,12 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun MainActivityContent() {
+fun MainActivityContent(isShouldShowOnboarding: Boolean = true) {
     /*
     コンポーズ可能な関数では、複数の関数によって読み取られるか変更される状態は、共通の祖先に配置される必要があります。
     そのためのプロセスを状態ホイスティングと呼びます。「ホイストする」とは、「持ち上げる」「昇格させる」といった意味です。
      */
-    var shouldShowOnboarding by remember { mutableStateOf(true) }
+    var shouldShowOnboarding by remember { mutableStateOf(isShouldShowOnboarding) }
 
     // ComposeTestThemeはTheme.ktで定義しているみたいだよ
     ComposeTestTheme {
@@ -70,7 +58,7 @@ fun MainActivityContent() {
                         shouldShowOnboarding = false
                     })
                 } else {
-                    ProfileContent()
+                    ProfileListScreen()
                 }
             }
         }
@@ -90,90 +78,16 @@ fun TopBar() {
     )
 }
 
-// Profile Content
+// `@Preview`アノテーションを使用すると、Android Studio内でコンポーズ可能な関数をプレビューできます
+@Preview(showBackground = true)
 @Composable
-fun ProfileContent(isExpanded: Boolean = false) {
-    // Jetpack Composeのrememberは、コンポーザブル関数内で状態を保持するために非常に重要な関数です
-    var expandedState by remember { mutableStateOf(isExpanded) }
-
-    // Row -> 横方向に整列できるよ
-    Row(modifier = Modifier.padding(8.dp)) {
-        Column {
-            // 画像を表示する
-            Image(
-                painter = painterResource(R.drawable.icon),
-                contentDescription = "Contact profile picture",
-                modifier = Modifier
-                    .size(100.dp)
-                    .clip(CircleShape)
-            )
-        }
-
-        // 幅8dpの空白を挿入
-        Spacer(modifier = Modifier.width(8.dp))
-
-        // Column -> 縦方向に整列できるよ
-        Column {
-            Column {
-                Row {
-                    Column(modifier = Modifier.weight(1f)) {
-                        // 備考: Typographyは自分で設定できる 詳細はTypeを参照
-
-                        Text(
-                            text = "Your Name",
-                            style = MaterialTheme.typography.titleLarge,
-                            modifier = Modifier.fillMaxWidth(),
-                        )
-                        Text(
-                            text = "@your_id",
-                            style = MaterialTheme.typography.titleSmall,
-                            modifier = Modifier.fillMaxWidth(),
-                        )
-                    }
-
-                    Spacer(modifier = Modifier.width(8.dp))
-
-                    Button(onClick = {
-                        expandedState = !expandedState
-                    }) {
-                        Text(text = if (expandedState) "Show less" else "Show more")
-                    }
-                }
-            }
-
-            Spacer(modifier = Modifier.height(8.dp))
-
-            if (expandedState) {
-                Text(
-                    text = "Your description.\n".repeat(4),
-                    modifier = Modifier.fillMaxWidth(),
-                )
-            }
-        }
-    }
+fun MainActivityPreviewShouldShowOnboarding() {
+    MainActivityContent(isShouldShowOnboarding = true)
 }
 
 // `@Preview`アノテーションを使用すると、Android Studio内でコンポーズ可能な関数をプレビューできます
 @Preview(showBackground = true)
 @Composable
 fun MainActivityPreview() {
-    MainActivityContent()
-}
-
-// `@Preview`アノテーションを使用すると、Android Studio内でコンポーズ可能な関数をプレビューできます
-@Preview(showBackground = true)
-@Composable
-fun ProfileContentPreview() {
-    ComposeTestTheme {
-        ProfileContent(isExpanded = false)
-    }
-}
-
-// `@Preview`アノテーションを使用すると、Android Studio内でコンポーズ可能な関数をプレビューできます
-@Preview(showBackground = true)
-@Composable
-fun ProfileContentPreviewExpanded() {
-    ComposeTestTheme {
-        ProfileContent(isExpanded = true)
-    }
+    MainActivityContent(isShouldShowOnboarding = false)
 }
---
+++ app/src/main/java/dev/ctsetera/composetest/ui/screen/ProfileListScreen.kt
@@ -0,0 +1,204 @@
+package dev.ctsetera.composetest.ui.screen
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.itemsIndexed
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material3.Button
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.painter.Painter
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import dev.ctsetera.composetest.R
+import dev.ctsetera.composetest.ui.theme.ComposeTestTheme
+
+data class ProfileItem(
+    val id: String,
+    val name: String,
+    val description: String,
+    val icon: Painter,
+)
+
+@Composable
+fun ProfileListScreen() {
+    val profileList = listOf(
+        ProfileItem(
+            id = "@potofu",
+            name = "霜月ポトフ",
+            description = buildString {
+                append("ニコニコ生放送でお絵かき配信をしているpepepeさんのオリジナルキャラクター。")
+                append("pepepeさんがフォトショップのフリーズにも負けずに書き上げたpepepeさんの妹である。")
+                append("ポトフちゃんはハーフだがpepepeさんはハーフではない。ふしぎ！")
+                append("おとなしくてSな照れ屋でひきこもりぎみで、家でお絵かき配信をしたり、同人サークルで稼いでいるらしい。")
+                append("ちなみにぱんつはくまさんぱんつ。")
+                append("最近お父さんが保護してきたワトラちゃんという友達が増えた。やったね！ポトフちゃん！")
+                append("配信ではポトフちゃんが絵を描き、ワトラちゃんはモデル、pepepeさんがおしゃべりを担当との事。")
+                append("お父さん（フランス人）も一緒に住んでおり、リストラ朝スーツを着て出かけて公園のベンチでボーっとして夜に帰ってくる仕事をしている。なお、お父さんはシャイで家族間の会話もメールで行っている。")
+                append("お母さん（日本人）は物心ついたときには家におらず、生活費はかかさず送られてきている模様。")
+                append("pepepeさん曰くどんどん描いてもいいのよ?との事。れっつどろーいんぐ！")
+            },
+            icon = painterResource(R.drawable.icon_potofu),
+        ),
+        ProfileItem(
+            id = "@watora",
+            name = "葉月ワトラ",
+            description = buildString {
+                append("pepepe（絵師）さんのオリジナルキャラクター。")
+                append("設定：")
+                append("赤髪のツインテール。アホ毛が生えていて、Ｗ形の髪飾りを付けている。")
+                append("性格はぶっきらぼうな感じ。父は鳥取、母は島根県出身。")
+                append("リアル草食系。裸パーカーのチャックは壊れている。　一人称はワタイ。")
+                append("生放送でワロタをワトラと打ち間違えたコメントがどんどん広がり、とうとうキャラクターになってしまった。ワトラｗｗｗｗｗｗ")
+            },
+            icon = painterResource(R.drawable.icon_watora)
+        ),
+    )
+
+    ProfileList(profileList = profileList)
+}
+
+// Profile List
+@Composable
+fun ProfileList(profileList: List<ProfileItem>) {
+    LazyColumn {
+        itemsIndexed(items = profileList) { index, item ->
+            ProfileContent(
+                profile = item,
+                isFirstItem = index == 0
+            )
+        }
+    }
+}
+
+// Profile Content
+@Composable
+fun ProfileContent(
+    profile: ProfileItem,
+    isExpanded: Boolean = false,
+    isFirstItem: Boolean = true,
+) {
+    // Jetpack Composeのrememberは、コンポーザブル関数内で状態を保持するために非常に重要な関数です
+    var expandedState by remember { mutableStateOf(isExpanded) }
+
+    // Row -> 横方向に整列できるよ
+    Row(
+        modifier = if (isFirstItem) {
+            Modifier.padding(8.dp)
+        } else {
+            Modifier.padding(
+                top = 0.dp,
+                start = 8.dp,
+                end = 8.dp,
+                bottom = 8.dp
+            )
+        }
+    ) {
+        Column {
+            // 画像を表示する
+            Image(
+                painter = profile.icon,
+                contentDescription = "Contact profile picture",
+                modifier = Modifier
+                    .size(100.dp)
+                    .clip(CircleShape)
+            )
+        }
+
+        // 幅8dpの空白を挿入
+        Spacer(modifier = Modifier.width(8.dp))
+
+        // Column -> 縦方向に整列できるよ
+        Column {
+            Column {
+                Row {
+                    Column(modifier = Modifier.weight(1f)) {
+                        // 備考: Typographyは自分で設定できる 詳細はTypeを参照
+
+                        Text(
+                            text = profile.name,
+                            style = MaterialTheme.typography.titleLarge,
+                            modifier = Modifier.fillMaxWidth(),
+                        )
+                        Text(
+                            text = profile.id,
+                            style = MaterialTheme.typography.titleSmall,
+                            modifier = Modifier.fillMaxWidth(),
+                        )
+                    }
+
+                    Spacer(modifier = Modifier.width(8.dp))
+
+                    Button(onClick = {
+                        expandedState = !expandedState
+                    }) {
+                        Text(text = if (expandedState) "Show less" else "Show more")
+                    }
+                }
+            }
+
+            Spacer(modifier = Modifier.height(8.dp))
+
+            if (expandedState) {
+                Text(
+                    text = profile.description,
+                    modifier = Modifier.fillMaxWidth(),
+                )
+            }
+        }
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun ProfileListScreenPreview() {
+    ComposeTestTheme {
+        ProfileListScreen()
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun ProfileContentPreview() {
+    ComposeTestTheme {
+        ProfileContent(
+            profile = ProfileItem(
+                id = "@your_id",
+                name = "Your Name",
+                description = "Your description.\n".repeat(4),
+                icon = painterResource(R.drawable.icon),
+            ), isExpanded = false
+        )
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun ProfileContentPreviewExpanded() {
+    ComposeTestTheme {
+        ProfileContent(
+            profile = ProfileItem(
+                id = "@your_id",
+                name = "Your Name",
+                description = "Your description.\n".repeat(4),
+                icon = painterResource(R.drawable.icon),
+            ), isExpanded = true
+        )
+    }
+}
